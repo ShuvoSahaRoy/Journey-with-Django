7d44bd121a2cd829b86209fa1796b9fcc224bbb7
Shuvo Saha Roy <sshuvo548@gmail.com>
2020-09-18 04:43:58 +0600

    n

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b%o1fil48xjuiij__wpfjnv=l(3ejj&%p!+y!xpynmao8%9#t*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ssroydjangoblog.herokuapp.com']
 
