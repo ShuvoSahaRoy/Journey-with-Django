7a9c43241b3b5cbf4c894e7cd7279b3bab685ed8
Shuvo Saha Roy <sshuvo548@gmail.com>
2020-09-18 22:03:06 +0600

    correction

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '../static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
